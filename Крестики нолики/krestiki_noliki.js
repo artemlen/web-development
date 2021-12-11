a03bff2045eca54006eaf2c5183778fd4458dbf8
artemlen <90240896+artemlen@users.noreply.github.com>
2021-12-11 13:05:05 +0300

    Add files via upload

---
+++ Крестики нолики/krestiki_noliki.js
@@ -0,0 +1,381 @@
+let permennaya , button ;
+
+//Функия для 1 клетки
+
+//переменная для подсчета ходов
+let move = 0;
+
+
+
+function  cl_1 ()
+{ 
+  //последний ход (крестик или нолик)      
+  last_word = document.getElementById("last_word").innerHTML;
+
+  //в button записываем значение кнопки
+  button = document.getElementById("id_1").innerHTML;
+
+  //Если пустая кнопка, то в переменную записываем "х" или "0" в зависимости от значения last_word
+  if(button === "") 
+  {
+    if(last_word == "x")
+    {
+    permennaya = "0"
+    } 
+    else 
+    {
+    permennaya = "x";
+    }
+    move += 1
+    
+  //меняем значение кнопки 
+  document.getElementById("id_1").innerHTML  = permennaya; 
+
+  //меняем значение последней фигуры (крестих или нолик)
+  document.getElementById("last_word").innerHTML  = permennaya; 
+ 
+  }
+  winner();     
+}
+
+function  cl_2 ()
+{ 
+  //последний ход (крестик или нолик)      
+  last_word = document.getElementById("last_word").innerHTML;
+
+  //в button записываем значение кнопки
+  button = document.getElementById("id_2").innerHTML;
+
+  //Если пустая кнопка, то в переменную записываем "х" или "0" в зависимости от значения last_word
+  if(button === "") 
+  {
+    if(last_word == "x")
+    {
+    permennaya = "0"
+    } 
+    else 
+    {
+    permennaya = "x";
+    }
+    move += 1
+    
+  //меняем значение кнопки 
+  document.getElementById("id_2").innerHTML  = permennaya; 
+
+  //меняем значение последней фигуры (крестих или нолик)
+  document.getElementById("last_word").innerHTML  = permennaya; 
+ 
+  }
+  winner();     
+}
+
+function  cl_3 ()
+{ 
+  //последний ход (крестик или нолик)      
+  last_word = document.getElementById("last_word").innerHTML;
+
+  //в button записываем значение кнопки
+  button = document.getElementById("id_3").innerHTML;
+
+  //Если пустая кнопка, то в переменную записываем "х" или "0" в зависимости от значения last_word
+  if(button === "") 
+  {
+    if(last_word == "x")
+    {
+    permennaya = "0"
+    } 
+    else 
+    {
+    permennaya = "x";
+    }
+    move += 1
+    
+  //меняем значение кнопки 
+  document.getElementById("id_3").innerHTML  = permennaya; 
+
+  //меняем значение последней фигуры (крестих или нолик)
+  document.getElementById("last_word").innerHTML  = permennaya; 
+ 
+  }
+  winner();     
+}
+
+function  cl_4 ()
+{ 
+  //последний ход (крестик или нолик)      
+  last_word = document.getElementById("last_word").innerHTML;
+
+  //в button записываем значение кнопки
+  button = document.getElementById("id_4").innerHTML;
+
+  //Если пустая кнопка, то в переменную записываем "х" или "0" в зависимости от значения last_word
+  if(button === "") 
+  {
+    if(last_word == "x")
+    {
+    permennaya = "0"
+    } 
+    else 
+    {
+    permennaya = "x";
+    }
+    move += 1
+    
+  //меняем значение кнопки 
+  document.getElementById("id_4").innerHTML  = permennaya; 
+
+  //меняем значение последней фигуры (крестих или нолик)
+  document.getElementById("last_word").innerHTML  = permennaya; 
+ 
+  }
+  winner();     
+}
+
+function  cl_5 ()
+{ 
+  //последний ход (крестик или нолик)      
+  last_word = document.getElementById("last_word").innerHTML;
+
+  //в button записываем значение кнопки
+  button = document.getElementById("id_5").innerHTML;
+
+  //Если пустая кнопка, то в переменную записываем "х" или "0" в зависимости от значения last_word
+  if(button === "") 
+  {
+    if(last_word == "x")
+    {
+    permennaya = "0"
+    } 
+    else 
+    {
+    permennaya = "x";
+    }
+    move += 1
+    
+  //меняем значение кнопки 
+  document.getElementById("id_5").innerHTML  = permennaya; 
+
+  //меняем значение последней фигуры (крестих или нолик)
+  document.getElementById("last_word").innerHTML  = permennaya; 
+ 
+  }
+  winner();     
+}
+
+function  cl_6 ()
+{ 
+  //последний ход (крестик или нолик)      
+  last_word = document.getElementById("last_word").innerHTML;
+
+  //в button записываем значение кнопки
+  button = document.getElementById("id_6").innerHTML;
+
+  //Если пустая кнопка, то в переменную записываем "х" или "0" в зависимости от значения last_word
+  if(button === "") 
+  {
+    if(last_word == "x")
+    {
+    permennaya = "0"
+    } 
+    else 
+    {
+    permennaya = "x";
+    }
+    move += 1
+    
+  //меняем значение кнопки 
+  document.getElementById("id_6").innerHTML  = permennaya; 
+
+  //меняем значение последней фигуры (крестих или нолик)
+  document.getElementById("last_word").innerHTML  = permennaya; 
+ 
+  }
+  winner();     
+}
+
+function  cl_7 ()
+{ 
+  //последний ход (крестик или нолик)      
+  last_word = document.getElementById("last_word").innerHTML;
+
+  //в button записываем значение кнопки
+  button = document.getElementById("id_7").innerHTML;
+
+  //Если пустая кнопка, то в переменную записываем "х" или "0" в зависимости от значения last_word
+  if(button === "") 
+  {
+    if(last_word == "x")
+    {
+    permennaya = "0"
+    } 
+    else 
+    {
+    permennaya = "x";
+    }
+    move += 1
+    
+  //меняем значение кнопки 
+  document.getElementById("id_7").innerHTML  = permennaya; 
+
+  //меняем значение последней фигуры (крестих или нолик)
+  document.getElementById("last_word").innerHTML  = permennaya; 
+ 
+  }
+  winner();     
+}
+
+function  cl_8 ()
+{ 
+  //последний ход (крестик или нолик)      
+  last_word = document.getElementById("last_word").innerHTML;
+
+  //в button записываем значение кнопки
+  button = document.getElementById("id_8").innerHTML;
+
+  //Если пустая кнопка, то в переменную записываем "х" или "0" в зависимости от значения last_word
+  if(button === "") 
+  {
+    if(last_word == "x")
+    {
+    permennaya = "0"
+    } 
+    else 
+    {
+    permennaya = "x";
+    }
+    move += 1
+    
+  //меняем значение кнопки 
+  document.getElementById("id_8").innerHTML  = permennaya; 
+
+  //меняем значение последней фигуры (крестих или нолик)
+  document.getElementById("last_word").innerHTML  = permennaya; 
+ 
+  }
+  winner();     
+}
+
+function  cl_9 ()
+{ 
+  //последний ход (крестик или нолик)      
+  last_word = document.getElementById("last_word").innerHTML;
+
+  //в button записываем значение кнопки
+  button = document.getElementById("id_9").innerHTML;
+
+  //Если пустая кнопка, то в переменную записываем "х" или "0" в зависимости от значения last_word
+  if(button === "") 
+  {
+    if(last_word == "x")
+    {
+    permennaya = "0"
+    } 
+    else 
+    {
+    permennaya = "x";
+    }
+    move += 1
+    
+  //меняем значение кнопки 
+  document.getElementById("id_9").innerHTML  = permennaya; 
+
+  //меняем значение последней фигуры (крестих или нолик)
+  document.getElementById("last_word").innerHTML  = permennaya; 
+ 
+  }
+  winner();     
+}
+
+function winner() {
+
+    // в allButtons записываем все кнопки класса block, то есть все наши поля
+    let allButtons =  document.getElementsByClassName("block");
+    console.log(allButtons);
+    
+    //если в одной из трех строк все кнопки "х" (победа)
+    if(allButtons[0].innerHTML =='x' & allButtons[1].innerHTML =='x' & allButtons[2].innerHTML =='x'){
+        alert('Победили крестики'); 
+        location.reload(); 
+    } 
+    else if(allButtons[3].innerHTML =='x' & allButtons[4].innerHTML =='x' & allButtons[5].innerHTML =='x'){
+        alert('Победили крестики'); 
+        location.reload(); 
+    }  
+    else if(allButtons[6].innerHTML =='x' & allButtons[7].innerHTML =='x' & allButtons[8].innerHTML =='x'){
+        alert('Победили крестики'); 
+        location.reload(); 
+    }
+    
+    //если в одном из трех столбцов все кнопки "х" (победа)
+    else if(allButtons[0].innerHTML =='x' & allButtons[3].innerHTML =='x' & allButtons[6].innerHTML =='x'){
+        alert('Победили крестики'); 
+        location.reload(); 
+    } 
+    else if(allButtons[1].innerHTML =='x' & allButtons[4].innerHTML =='x' & allButtons[7].innerHTML =='x'){
+        alert('Победили крестики'); 
+        location.reload(); 
+    }  
+    else if(allButtons[2].innerHTML =='x' & allButtons[5].innerHTML =='x' & allButtons[8].innerHTML =='x'){
+        alert('Победили крестики'); 
+        location.reload(); 
+    }
+    
+    //если на одной из диагоналей все кнопки "х" (победа)
+    else if(allButtons[0].innerHTML =='x' & allButtons[4].innerHTML =='x' & allButtons[8].innerHTML =='x'){
+        alert('Победили крестики'); 
+        location.reload(); 
+    } 
+    else if(allButtons[2].innerHTML =='x' & allButtons[4].innerHTML =='x' & allButtons[6].innerHTML =='x'){
+        alert('Победили крестики'); 
+        location.reload(); 
+    }  
+    
+    //если в одной из трех строк все кнопки "0" (победа)
+    else if(allButtons[0].innerHTML =='0' & allButtons[1].innerHTML =='0' & allButtons[2].innerHTML =='0'){
+        alert('Победили нолики'); 
+        location.reload(); 
+    } 
+    else if(allButtons[3].innerHTML =='0' & allButtons[4].innerHTML =='0' & allButtons[5].innerHTML =='0'){
+        alert('Победили нолики'); 
+        location.reload(); 
+    }  
+    else if(allButtons[6].innerHTML =='0' & allButtons[7].innerHTML =='0' & allButtons[8].innerHTML =='0'){
+        alert('Победили нолики'); 
+        location.reload(); 
+    }
+    
+     //если в одном из трех столбцов все кнопки "0" (победа)
+     else if(allButtons[0].innerHTML =='0' & allButtons[3].innerHTML =='0' & allButtons[6].innerHTML =='0'){
+        alert('Победили нолики'); l
+        ocation.reload(); 
+    } 
+    else if(allButtons[1].innerHTML =='0' & allButtons[4].innerHTML =='0' & allButtons[7].innerHTML =='0'){
+        alert('Победили нолики'); 
+        location.reload(); 
+    }  
+    else if(allButtons[2].innerHTML =='0' & allButtons[5].innerHTML =='0' & allButtons[8].innerHTML =='0'){
+        alert('Победили нолики'); 
+        location.reload(); 
+    }
+    
+        //если на одной из диагоналей все кнопки "0" (победа)
+    else if(allButtons[0].innerHTML =='0' & allButtons[4].innerHTML =='0' & allButtons[8].innerHTML =='0'){
+        alert('Победили нолики'); 
+        location.reload(); 
+    } 
+    else if(allButtons[2].innerHTML =='0' & allButtons[4].innerHTML =='0' & allButtons[6].innerHTML =='0'){
+        alert('Победили нолики'); 
+        location.reload(); 
+    }
+
+    else if (move == 9) {
+        alert("Ничья");
+        location.reload();
+    }
+
+}
+
+
+
+
+
